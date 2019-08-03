186377b0cd48e0d61efb71dd3d8d36418b80ac5a
powerengine <1025924049@qq.com>
2019-08-03 22:37:29 +0800

    第一次提交

---
+++ shop-basic/shop-basic-eureka/src/test/java/com/ma/shop/basic/shopbasiceureka/ShopBasicEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ma.shop.basic.shopbasiceureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopBasicEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
